b197630c0349164173ad6b92bb94c67ddc9b6cc6
Nadilson <nadilson@protonmail.com>
2022-10-12 05:01:32 -0300

    Componente Heading e ajustes no componente Text

--- src/components/Heading.stories.tsx
+++ src/components/Heading.stories.tsx
@@ -1,12 +1,13 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { Text, TextProps } from "./Text";
+import { Heading, HeadingProps } from "./Heading";
 
 
 export default {
-    title: 'Components/Text',
-    component: Text,
+    title: 'Components/Heading',
+    component: Heading,
     args: {
         children: 'Lorem ipsum.',
+        size: 'md',
     },
 
     argTypes: {
@@ -15,42 +16,42 @@ export default {
             control: {
                 type: 'inline-radio'
             }
-        },
-        asChild: {
-            table: {
-                disable: true,
-            }
         }
     }
-} as Meta<TextProps>
+} as Meta<HeadingProps>
 
 
-export const Default: StoryObj<TextProps> = {
+export const Default: StoryObj<HeadingProps> = {
     
 }
 
-export const Small: StoryObj<TextProps> = {
+export const Small: StoryObj<HeadingProps> = {
     args: {
         size: 'sm',
     }
 }
 
-export const Large: StoryObj<TextProps> = {
+export const Large: StoryObj<HeadingProps> = {
     args: {
         size: 'lg',
     }
 }
 
-export const CustomComponent: StoryObj<TextProps> = {
+export const CustomComponent: StoryObj<HeadingProps> = {
     args: {
         asChild: true,
         children: (
-            <p>Text with P tag</p>
+            <h1>Heading with H1</h1>
         )
 
     },
     argTypes: {
         children: {
+            table: {
+                disable: true,
+            }
+        },
+        asChild: {
             table: {
                 disable: true,
             }
