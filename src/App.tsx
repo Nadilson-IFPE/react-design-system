6675807a38062dbe1864a0027a34ba5bef1bbb09
Nadilson <nadilson@protonmail.com>
2022-10-12 00:14:26 -0300

    Tailwindcss, Postcss, autoprefixer, font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import './styles/global.css';
+
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 className='font-bold text-5xl text-violet-500'>Hello World</h1>
+    <button className='bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600'>
+      Enviar
+    </button>
     </div>
   );
 }
