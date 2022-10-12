32a0447d160753db5198e3661daedc19fa058176
Nadilson <nadilson@protonmail.com>
2022-10-12 01:35:11 -0300

    Configuração do Storybook e do Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1 className='font-bold text-5xl text-violet-500'>Hello World</h1>
-    <button className='bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600'>
+    <button className='bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300'>
       Enviar
     </button>
     </div>
