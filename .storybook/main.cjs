1dc2b4a022beed515828dd068910921964de750f
Nadilson <nadilson@protonmail.com>
2022-10-13 04:39:24 -0300

    Fix no comnponente Checkbox, config adicional (bug fix no Vite) no Storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,18 +1,21 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
-  },
-  "features": {
-    "storyStoreV7": true
-  }
+    "stories": [
+        "../src/**/*.stories.mdx",
+        "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    ],
+    "addons": [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions"
+    ],
+    "framework": "@storybook/react",
+    "core": {
+        "builder": "@storybook/builder-vite"
+    },
+    "typescript": {
+        "reactDocgen": "react-docgen"
+    },
+    "features": {
+        "storyStoreV7": true
+    }
 }
--- src/components/Checkbox.stories.tsx
+++ src/components/Checkbox.stories.tsx
@@ -14,7 +14,7 @@ export default {
             return (
                 <div className='flex items-center gap-2'>
                     {Story()}
-                    <Text size="sm">Lembrar de mim por 30 dias</Text>
+                    <Text size="sm" asChild={false}>Lembrar de mim por 30 dias</Text>
                 </div>
             )
         }
