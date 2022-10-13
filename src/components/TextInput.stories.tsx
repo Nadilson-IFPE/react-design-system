937e2ddb1f55c8db30b351983f22a758c0410581
Nadilson <nadilson@protonmail.com>
2022-10-13 00:29:20 -0300

    Finalização do componente TextIput

--- src/components/TextInput.stories.tsx
+++ src/components/TextInput.stories.tsx
@@ -1,18 +1,33 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { TextInput, TextInputProps } from './TextInput';
+import { Envelope } from 'phosphor-react';
+import { TextInput, TextInputRootProps } from './TextInput';
 
 
 export default {
     title: 'Components/TextInput',
-    component: TextInput,
-    args: { 
-        placeholder: 'Type your e-mail address...' 
+    component: TextInput.Root,
+    args: {
+        children: [
+            <TextInput.Icon>
+                <Envelope />
+            </TextInput.Icon>,
+          <TextInput.Input placeholder="Type your e-mail address..." />
+        ],
     },
+    argTypes: {
+        children: {
+            table: {
+                disable: true,
+            }
+        }        
+    }
+} as Meta<TextInputRootProps>
 
-    argTypes: {}
-} as Meta<TextInputProps>
 
+export const Default: StoryObj<TextInputRootProps> = {}
 
-export const Default: StoryObj<TextInputProps> = {
-    
+export const WithoutIcon: StoryObj<TextInputRootProps> = {
+    args: {
+        children: <TextInput.Input placeholder="Type your e-mail address..." />
+    }
 }
--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,14 +1,47 @@
-import {clsx} from "clsx";
-import { InputHTMLAttributes } from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { InputHTMLAttributes, ReactNode } from "react";
 
-export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
-    placeholder?: string;
+export interface TextInputRootProps {
+    children: ReactNode;
 }
 
-export function TextInput(props: TextInputProps) {
+function TextInputRoot(props: TextInputRootProps) {
     return (
-        <input 
-        className={clsx("py-4 px-3 rounded outline-none bg-gray-800 w-full text-gray-100 tex-xs placeholder:text-gray-400 focus:ring-2 ring-cyan-300",
-        )}{...props} />
+        <div className="flex items-center gap-3 h-12 py-4 px-3 bg-gray-800 w-full rounded focus-within:ring-2 ring-cyan-300">
+            {props.children}
+        </div>
     )
+}
+
+TextInputRoot.displayName = "TextInput.Root";
+
+export interface TextInputIconProps {
+    children: ReactNode;
+}
+
+function TextInputIcon(props: TextInputIconProps) {
+    return (
+        <Slot className="w-6 h-6 text-gray-400">
+            {props.children}
+        </Slot>
+    )
+}
+
+TextInputIcon.displayName = "TextInput.Icon";
+
+export interface TextInputInputProps extends InputHTMLAttributes<HTMLInputElement> { }
+
+function TextInputInput(props: TextInputInputProps) {
+    return (
+        <input
+            className="bg-transparent flex-1 bg-gray-800 w-full text-gray-100 tex-xs placeholder:text-gray-400 outline-none"
+            {...props}
+        />
+    )
+}
+
+export const TextInput = {
+    Root: TextInputRoot,
+    Input: TextInputInput,
+    Icon: TextInputIcon,
 }
